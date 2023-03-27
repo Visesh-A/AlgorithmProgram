714fdeb83176a273bc5726859d17dff487125e75
“Visesh-A” <viseshajl@gmail.com>
2023-03-27 17:22:00 +0530

    [visesh]Add:Unordered List

--- AlgorithmPrograms/Program.cs
+++ AlgorithmPrograms/Program.cs
@@ -2,6 +2,7 @@
 using AlgorithmPrograms.BinarySearch;
 using AlgorithmPrograms.BubbleSort;
 using AlgorithmPrograms.CustomizeMessage;
+using AlgorithmPrograms.DataStructureProblems;
 using AlgorithmPrograms.InsertionSort;
 using System;
 namespace AlgorithmPrograms
@@ -14,7 +15,7 @@ namespace AlgorithmPrograms
             while (flag)
             {
                 Console.WriteLine("\nChoose Option to perform\n1.Binary Search for Word \n2.Insertion Sort \n3.Bubble sort " +
-                     "\n4.Anagram \n5.Prime Range NUmber \n6.Customize Message \n7.Exit");
+                     "\n4.Anagram \n5.Prime Range NUmber \n6.Customize Message \n7.UnOrder List(Data Structure Problems) \n8.Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -42,7 +43,10 @@ namespace AlgorithmPrograms
                         CustomizeDetails customizeDetails = new CustomizeDetails();
                         customizeDetails.Customize();
                         break;
-                    case 7:
+                        UnOrderList unOrderList = new UnOrderList();
+                        unOrderList.ReadFile(@"C:\Users\VISESH\Documents\bl excercise\AlgorithmProgram\AlgorithmPrograms\DataStructureProblems\UnOrderList.cs");
+                        break;
+                    case 8:
                         flag = false;
                         break;
                 }
