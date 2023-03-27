e0eeecbffce8554982b13bce5d5fb3d84fee717e
“Visesh-A” <viseshajl@gmail.com>
2023-03-27 16:28:03 +0530

    [visesh]Add:Binary Word Search

---
+++ AlgorithmPrograms/Program.cs
@@ -0,0 +1,13 @@
+﻿using AlgorithmPrograms.BinarySearch;
+using System;
+namespace AlgorithmPrograms
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            BinaryWrodSearch search = new BinaryWrodSearch();
+            search.ReadFile(@"C:\Users\VISESH\Documents\bl excercise\AlgorithmProgram\AlgorithmPrograms\BinaryWordSearch\File.txt");
+        }
+    }
+}
